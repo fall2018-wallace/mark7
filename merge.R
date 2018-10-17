c77b08ba4cf6a4ad3d6a4f124ebaa169538eaa80
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 16:03:22 -0400

    Saving merge.R

--- merge.R
+++ merge.R
@@ -1,1 +1,17 @@
 
+#str(dfCleanCensus)
+#View(dfCleanCensus)
+CensusData<-dfCleanCensus
+ArrestsData <- USArrests
+
+
+
+ArrestsData=cbind(rownames(ArrestsData),ArrestsData) #Combine sequence of dataframe by rows or columns
+colnames(ArrestsData)=c("stateName") #Giving name to the new column
+ArrestsData
+
+#3)	Create a merged dataframe -- with the attributes from both dataframes
+MergedData=merge(ArrestsData,CensusData,by="stateName") #merging the two dataframes on concolumn name "stateName"
+colnames(MergedData)=c("stateName","Murder","Assault","UrbanPop","Rape","population","popOver18","percentOver18")
+MergedData
+
