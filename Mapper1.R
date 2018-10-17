4a7743c8aac0635b9ef8d8664bc4fdaa59bbb1ad
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 16:45:56 -0400

    Saving Mapper1.R

--- Mapper1.R
+++ Mapper1.R
@@ -16,3 +16,7 @@ USmur <- ggplot(mergeDf, aes(map_id = stateName))
 USmur <- USmur + geom_map(map = us, aes(fill=Murder))
 USmur <- USmur + expand_limits(x = mergeDf$x, y= mergeDf$y)
 USmur <- USmur + coord_map() + ggtitle("Map of US based on murder rate of states")
+
+
+#5)	 Show the population as a circle per state (the larger the population, the larger the circle), using the location defined by the center of each state
+USmurCir <- USmur + geom_point(data = mergeDf, aes(x = mergeDf$x, y= mergeDf$y, color = "red", size = mergeDf$population))     #geom_point() plots points at the center of every state. Size attribute controls the size of the points
