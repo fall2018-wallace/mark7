a47c72a94e3e3f702d776cc1ffeb972fcc8e2631
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-17 16:44:30 -0400

    Saving Mapper1.R

--- Mapper1.R
+++ Mapper1.R
@@ -4,17 +4,15 @@
 library("ggmap")
 library("ggplot2")
 us <- map_data("state")     #turn data from the maps package in to a data frame suitable for plotting with ggplot2
-us
-mapArea <- ggplot(mergeDf, aes(map_id = stateName))     #initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName.
-mapArea <- mapArea + geom_map(map = us, aes(fill= stateArea))     #creating a map visualization
-mapArea <- mapArea + expand_limits(x = mergeDf$x, y= mergeDf$y)     #defining the x and y axes values of the map
-mapArea <- mapArea + coord_map() + ggtitle("Map of US based on state area")     #coord_map() handles the distortion and aspect ratio of the map. ggtitle() gives a title to the map
-mapArea
+USmap <- ggplot(mergeDf, aes(map_id = stateName))     #initializing  a ggplot object and passing mergeDf as the input data with map ID as stateName.
+USmap <- USmap + geom_map(map = us, aes(fill= stateArea))     #creating a map visualization
+USmap <- USmap + expand_limits(x = mergeDf$x, y= mergeDf$y)     #defining the x and y axes values of the map
+USmap <- USmap + coord_map() + ggtitle("Map of US based on state area")     #coord_map() handles the distortion and aspect ratio of the map. ggtitle() gives a title to the map
+
 
 #Step C: Create a color shaded map of the U.S. based on the Murder rate for each state 
 #4)	Repeat step B, but color code the map based on the murder rate of each state.
-mapArea1 <- ggplot(mergeDf, aes(map_id = stateName))
-mapArea1 <- mapArea1 + geom_map(map = us, aes(fill=Murder))
-mapArea1 <- mapArea1 + expand_limits(x = mergeDf$x, y= mergeDf$y)
-mapArea1 <- mapArea1 + coord_map() + ggtitle("Map of US based on murder rate of states")
-mapArea1
+USmur <- ggplot(mergeDf, aes(map_id = stateName))
+USmur <- USmur + geom_map(map = us, aes(fill=Murder))
+USmur <- USmur + expand_limits(x = mergeDf$x, y= mergeDf$y)
+USmur <- USmur + coord_map() + ggtitle("Map of US based on murder rate of states")
